6a4f4fe66e7fecffa9b0770b69a1860a7e8f3ed8
Douglas Santos <dsousasantos91@gmail.com>
2018-09-18 19:48:16 -0300

    Exercício 5.4 Elementos Estáticos

---
+++ Static/src/Contador.java
@@ -0,0 +1,18 @@
+
+public class Contador {
+	
+	public static int valor;
+	
+	static {
+		valor = 1;
+	}
+	
+	public static void incrementar() {
+		valor++;
+	}
+
+	public static int getValor() {
+		return valor;
+	}
+	
+}
