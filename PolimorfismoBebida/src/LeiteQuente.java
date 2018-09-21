80ea11f6732727b4c1395d1823d6d1933b2bb879
Douglas Santos <dsousasantos91@gmail.com>
2018-09-21 18:58:38 -0300

    Exercício 6.5 Polimorfismo na prática

---
+++ PolimorfismoBebida/src/LeiteQuente.java
@@ -0,0 +1,14 @@
+
+public class LeiteQuente extends Bebida {
+	
+	public LeiteQuente() {
+		super("Leite quente", true);
+	}
+	
+	@Override
+	public void preparar() {
+		System.out.println("Pegando um copo");
+		System.out.println("Colocando o leito no copo");
+	}
+
+}
