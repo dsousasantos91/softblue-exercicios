44e937d057232b86987fcaec6296bddf402a581e
Douglas Santos <dsousasantos91@gmail.com>
2018-09-24 19:08:40 -0300

    Exercício 8.2 Tratando e lançando checked exceptions

---
+++ CheckedExceptions/src/Banco.java
@@ -0,0 +1,22 @@
+
+public class Banco {
+
+	public void realizarOperacao() throws BancoException {
+
+		ContaBancaria c = new ContaBancaria(1000);
+
+		try {
+			c.sacar(-50);
+			System.out.println("Saque realizado com sucesso");
+			System.out.println(c.getSaldo());
+		} catch (ValorNegativoException e) {
+			throw new BancoException("Erro de saque: valor negativo", e);
+			
+		} catch (SaldoInsuficienteException e) {
+			throw new BancoException("Erro de saque: saldo insuficiente", e);
+		
+		}
+		
+	}
+
+}
