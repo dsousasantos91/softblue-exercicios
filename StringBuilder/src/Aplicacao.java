afed92cb8ca4338189680c5196dd12711046e84c
Douglas de Sousa Santos <douglas.sousa@basis.com.br>
2018-12-27 13:48:33 -0200

    Datas, Time, Enums, Formataçao de numeros, String e StringBuilder

---
+++ StringBuilder/src/Aplicacao.java
@@ -0,0 +1,20 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < 1000; i++) {
+			sb.append("X");
+		}
+		
+		String s = sb.toString();
+		
+		System.out.println(s);
+	
+		System.out.println("A" + "B" + "C" + "D");
+		
+	}
+
+}
